f59dc5ad98642fabd0d9f9c8866e8235f151b447
Étienne Després <etienne@pochour.com>
2017-11-13 22:43:47 -0500

    Règle un bug qui survenait quand la base de données était vide

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -6,17 +6,23 @@ class LinesController < ApplicationController
   def show
     @lines = []
     line = params[:id].nil? ? Line.order("RANDOM()").first : Line.find(params[:id])
-    @lines << line
     
-    @line = Line.new(parent_id: line.id)
-    
-    while !line.parent_id.nil? && line.parent_id > 0
-      line = Line.find(line.parent_id)
-      @lines.unshift(line)
+    if line.nil?
+      redirect_to new_line_path
+    else
+      @lines << line
+  
+      @line = Line.new(parent_id: line.id)
+  
+      while !line.parent_id.nil? && line.parent_id > 0
+        line = Line.find(line.parent_id)
+        @lines.unshift(line)
+      end
     end
   end
 
   def new
+    @line = Line.new
   end
   
   def create
